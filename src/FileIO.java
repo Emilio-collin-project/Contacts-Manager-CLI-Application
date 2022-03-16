e26221c9916cf32e3a5e345822c006b1179bd37e
Emilio Benavides <emilio.benavides68@gmail.com>
2022-03-16 09:19:53 -0500

    added contacts.txt file

--- src/FileIO.java
+++ src/FileIO.java
@@ -1,10 +1,11 @@
-import com.sun.jdi.connect.IllegalConnectorArgumentsException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class FileIO {
     public static void main(String[] args) {
         List<String> contacts = new ArrayList<>(Arrays.asList("John", "Michael", "Erik"));
         System.out.println(strings);
-
 
     }
 }
