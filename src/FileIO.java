98745b7f11af1517884d2cda9ac81c135b382be3
Emilio Benavides <emilio.benavides68@gmail.com>
2022-03-15 20:59:55 -0500

    Started on Contacts-Manager project

---
+++ src/FileIO.java
@@ -0,0 +1,10 @@
+import com.sun.jdi.connect.IllegalConnectorArgumentsException;
+
+public class FileIO {
+    public static void main(String[] args) {
+        List<String> contacts = new ArrayList<>(Arrays.asList("John", "Michael", "Erik"));
+        System.out.println(strings);
+
+
+    }
+}
